db4ae8cae7fd443bcbd1e5916d3981dace8c4787
Lucas Lemanowicz <lucas@levro.com>
2024-12-02 18:58:49 -0500

    Add comments

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,32 @@
-import { H } from "highlight.run";
 import { Outlet } from "react-router";
+
+/*
+
+If you leave the code as is and run `pnpm build` the build will look like this:
+
+```
+dist/index.html                           0.46 kB │ gzip:   0.30 kB
+dist/assets/react-CHdo91hT.svg            4.13 kB │ gzip:   2.05 kB
+dist/assets/index-n_ryQ3BS.css            1.39 kB │ gzip:   0.71 kB
+dist/assets/index-DjMApXNi-BbQAe5Vu.js  355.20 kB │ gzip: 117.28 kB
+dist/assets/index-C-B63nXv.js           424.32 kB │ gzip: 128.17 kB
+```
+
+However if you remove the lines below, the build will be significantly smaller:
+
+```
+dist/index.html                   0.46 kB │ gzip:  0.30 kB
+dist/assets/react-CHdo91hT.svg    4.13 kB │ gzip:  2.05 kB
+dist/assets/index-n_ryQ3BS.css    1.39 kB │ gzip:  0.71 kB
+dist/assets/index-D8s4E482.js   215.73 kB │ gzip: 70.92 kB
+```
+
+Notice that even the original index file is smaller (not the one that is loaded async)
+
+*/
+
+// Highlight Start
+import { H } from "highlight.run";
 
 const HIGHLIGHT_PROJECT_ID = import.meta.env.VITE_HIGHLIGHT_PROJECT_ID;
 const BUILD_ID = import.meta.env.VITE_GIT_COMMIT_SHA;
@@ -15,10 +42,12 @@ if (HIGHLIGHT_PROJECT_ID != null) {
     version: BUILD_ID,
   });
 }
+// Highlight End
 
 export const Root: React.FC = () => {
   return (
     <div>
+      {/* Various Providers and other application wrappers here */}
       <Outlet />
     </div>
   );
